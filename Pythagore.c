f7ea24c1e02b9d3717a62b198e198b7391997a8b
Daymortel <34453670+Daymortel@users.noreply.github.com>
2021-10-10 11:10:11 +0200

    Initial commit

---
+++ Pythagore.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    // on récupère la valeur du côté ab
+    float ab = printf("valeur ab (en cm) : ");
+    scanf("%f", &ab);
+
+    // on récupère la valeur du côté ac
+    float ac = printf("valeur ac (en cm) : ");
+    scanf("%f", &ac);
+
+    // on calcule la valeur de l'hypoténuse
+    // bc² = ab² + ac²
+    float bc = pow(ab, 2) + pow(ac, 2);
+    printf("l'hypoténuse est de %.2f cm\n", sqrt(bc));
+}
